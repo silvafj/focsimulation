3409120925021de3a2e5d388582c5085da4b9aa2
Fernando Silva <fsilva.pt@gmail.com>
2019-07-24 10:28:34 +0100

    Added types to Noam, to be used from TypeScript. Noam is a JavaScript library for working with automata and formal grammars for regular and context-free languages.

--- src/components/RegularLanguage.tsx
+++ src/components/RegularLanguage.tsx
@@ -1,17 +1,36 @@
 import React from 'react';
 import {
     Layout,
-    Menu,
     Input,
 } from 'antd';
 
+import noam from 'noam';
+
 import './RegularLanguage.css';
 
+function generateAutomaton(expression: string, ref: React.Ref<HTMLDivElement>) {
+    const automaton: any = noam.fsm.convertStatesToNumbers(
+        noam.fsm.minimize(
+            noam.fsm.convertNfaToDfa(
+                noam.fsm.convertEnfaToNfa(
+                    noam.re.string.toAutomaton(expression)))));
+}
+
 const RegularLanguage: React.FC = () => {
+
+    const automatonRef: React.RefObject<HTMLDivElement> = React.createRef();
+
+    const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        console.log(event.target.value);
+        console.log(noam);
+        console.log(automatonRef);
+        generateAutomaton(event.target.value, automatonRef);
+    }
+
     return (
         <Layout>
             <h1>Regular language</h1>
-            <Input size="large" placeholder="What is your regular language expression?" />
+            <Input size="large" placeholder="What is your regular language expression?" onChange={onChange} />
             <h1>Test strings</h1>
             <div className="field accept">
                 <h2>Accept</h2>
@@ -21,6 +40,7 @@ const RegularLanguage: React.FC = () => {
                 <h2>Reject</h2>
                 <Input.TextArea rows={8} />
             </div>
+            <div ref={automatonRef} />
         </Layout>
     );
 }
