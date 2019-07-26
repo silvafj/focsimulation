92f80bd234a5fd0c2d9d0f10cda431d487242da2
Fernando Silva <fsilva.pt@gmail.com>
2019-07-26 14:34:26 +0100

    Add help about regular languages and indicator when the expression is invalid

--- src/components/RegularLanguage.tsx
+++ src/components/RegularLanguage.tsx
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import {
     Layout,
     Input,
+    Popover,
+    Tooltip,
+    Icon,
 } from 'antd';
 
 import noam from 'noam';
@@ -10,22 +13,37 @@ import { Module, render } from 'viz.js/full.render.js';
 
 import './RegularLanguage.css';
 
-function generateAutomaton(expression: string): any {
-    return noam.fsm.convertStatesToNumbers(
-        noam.fsm.minimize(
-            noam.fsm.convertNfaToDfa(
-                noam.fsm.convertEnfaToNfa(
-                    noam.re.string.toAutomaton(expression)))));
+function isValidAutomaton(automaton: any): boolean {
+    return automaton && automaton.transitions.length > 0;
 }
 
-function testStrings(automaton: any, strings: Array<string>, expected: boolean): string {
+function generateAutomaton(expression: string): any {
+    try {
+        const automaton = noam.fsm.convertStatesToNumbers(
+            noam.fsm.minimize(
+                noam.fsm.convertNfaToDfa(
+                    noam.fsm.convertEnfaToNfa(
+                        noam.re.string.toAutomaton(expression)))));
+
+        return [automaton, null];
+    } catch (error) {
+        if (error.name === "RegexError") {
+            return [null, error];
+        }
+    }
+
+    return;
+}
+
+
+function testWords(automaton: any, words: Array<string>, expected: boolean): string {
     var results: Array<string> = [];
 
-    if (automaton.transitions.length === 0) {
+    if (!isValidAutomaton(automaton)) {
         return '';
     }
 
-    for (let str of strings) {
+    for (let str of words) {
         var strState: string = expected ? 'invalid' : 'valid';
 
         try {
@@ -43,6 +61,10 @@ function testStrings(automaton: any, strings: Array<string>, expected: boolean):
 }
 
 function renderAutomaton(automaton: any, automatonParent: React.RefObject<HTMLDivElement>): void {
+    if (!isValidAutomaton(automaton)) {
+        return;
+    }
+
     (new Viz({ Module, render }))
         .renderSVGElement(noam.fsm.printDotFormat(automaton))
         .then((svgElement) => {
@@ -54,63 +76,99 @@ function renderAutomaton(automaton: any, automatonParent: React.RefObject<HTMLDi
                 automatonParent.current.removeChild(automatonParent.current.firstChild);
             }
 
-            if (automaton.transitions.length === 0) {
-                return;
+            if (isValidAutomaton(automaton)) {
+                automatonParent.current.appendChild(svgElement);
             }
-
-            automatonParent.current.appendChild(svgElement);
         })
         .catch(error => {
             console.error(error);
         });
 }
 
+function renderExpressionHelp(): any {
+    const title = <span>What is a regular expression?</span>;
+    const content = (
+        <div>
+            <p>A regular expression (over an alphabet Σ) is a string consisting of symbols from Σ, plus symbols from the list <em>+</em>, <em>*</em>, <em>(</em>, <em>)</em>, <em>$</em>.</p>
+            <ul>
+                <li><strong>+</strong> (alteration operator, ∪)</li>
+                <li><strong>∗</strong> (Kleene star); 0 or more symbols</li>
+                <li><strong>(</strong>; start grouping</li>
+                <li><strong>)</strong>; end grouping</li>
+                <li><strong>$</strong> (epsilon, ε); empty string</li>
+                <li><strong>\</strong> (backslash); used for escaping the special meaning of all the listed characters, including backslash itself</li>
+            </ul>
+
+            <p>Example: <code>a(a+b)*</code></p>
+            <p>= all words starting with a, followed by any word over {'{a, b}'}</p>
+            <p>= all words over the alphabet {'{a, b}'} starting with a</p>
+        </div>
+    );
+
+    return (
+        <Popover placement="rightTop" title={title} content={content}>
+            <Icon type="question-circle" style={{ color: 'rgba(0,0,0,.45)' }} />
+        </Popover>);
+}
+
+function renderExpressionStatus(error: any): any {
+    if (!error) {
+        return (<span></span>);
+    }
+
+    return (
+        <Tooltip placement="topRight" title={error.message}>
+            <Icon type="close-circle" theme="filled" style={{ color: 'red' }} />
+        </Tooltip>
+    );
+}
+
 const RegularLanguage: React.FC = () => {
     const [expression, setExpression] = useState('');
-    const [acceptStrings, setAcceptStrings] = useState('');
-    const [rejectStrings, setRejectStrings] = useState('');
+    const [acceptWords, setAcceptWords] = useState('');
+    const [rejectWords, setRejectWords] = useState('');
 
-    const acceptBackdrop: React.RefObject<HTMLDivElement> = React.createRef();
-    const rejectBackdrop: React.RefObject<HTMLDivElement> = React.createRef();
     const automatonParent: React.RefObject<HTMLDivElement> = React.createRef();
 
-    const automaton: any = generateAutomaton(expression);
+    const [automaton, error] = generateAutomaton(expression);
     renderAutomaton(automaton, automatonParent);
 
     return (
         <Layout>
-            <h1>Regular language</h1>
-            <Input size="large" placeholder="What is your regular language expression?"
+            <h1>Expression</h1>
+            <Input size="large" placeholder="Write your regular expression"
+                prefix={renderExpressionHelp()}
+                suffix={renderExpressionStatus(error)}
                 onChange={(event: React.ChangeEvent<HTMLInputElement>) => setExpression(event.target.value)} />
-            <h1>Test strings</h1>
+            <h1>Words</h1>
             <div className="field accept">
                 <h2>Accept</h2>
 
                 <div className="ht-container">
-                    <div className="ht-backdrop" ref={acceptBackdrop}>
+                    <div className="ht-backdrop">
                         <div className="ht-highlights" dangerouslySetInnerHTML={{
                             __html:
-                                testStrings(automaton, acceptStrings.split('\n'), true)
+                                testWords(automaton, acceptWords.split('\n'), true)
                         }}>
                         </div>
                     </div>
                     <Input.TextArea rows={8}
-                        onChange={(event: React.ChangeEvent<HTMLTextAreaElement>) => setAcceptStrings(event.target.value)}
+                        onChange={(event: React.ChangeEvent<HTMLTextAreaElement>) => setAcceptWords(event.target.value)}
                     />
                 </div>
             </div>
             <div className="field reject">
                 <h2>Reject</h2>
                 <div className="ht-container">
-                    <div className="ht-backdrop" ref={rejectBackdrop}>
+                    <div className="ht-backdrop">
                         <div className="ht-highlights" dangerouslySetInnerHTML={{
                             __html:
-                                testStrings(automaton, rejectStrings.split('\n'), false)
+                                testWords(automaton, rejectWords.split('\n'), false)
                         }}>
                         </div>
                     </div>
                     <Input.TextArea rows={8}
-                        onChange={(event: React.ChangeEvent<HTMLTextAreaElement>) => setRejectStrings(event.target.value)}
+                        onChange={(event: React.ChangeEvent<HTMLTextAreaElement>) => setRejectWords(event.target.value)}
                     />
                 </div>
             </div>
