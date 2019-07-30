96e76bd09d2bee9b2322a2c2369441a840a92bba
Fernando Silva <fsilva.pt@gmail.com>
2019-07-30 20:30:20 +0100

    Ability to link nodes

--- src/components/automaton-designer/automaton-designer.tsx
+++ src/components/automaton-designer/automaton-designer.tsx
@@ -4,10 +4,60 @@ import noam from 'noam';
 
 import './automaton-designer.css';
 
-const Link: React.FC<{}> = ({ }) => {
+function relativePositionToElement(target: Element, x: number, y: number) {
+    const rect = target.getBoundingClientRect();
+    return {
+        x: x - rect.left,
+        y: y - rect.top
+    };
+}
+
+function closestPointOnCircle(nX: number, nY: number, nR: number, x: number, y: number) {
+    var dx = x - nX;
+    var dy = y - nY;
+    var scale = Math.sqrt(dx * dx + dy * dy);
+    return {
+        fromX: nX + dx * nR / scale,
+        fromY: nY + dy * nR / scale,
+    };
+};
+
+
+const Edge: React.FC<{
+    automaton: any,
+    fromState: string,
+    toState?: string,
+    mouseX?: number,
+    mouseY?: number,
+    symbol?: string
+}> = ({ automaton, fromState, toState, mouseX, mouseY, symbol }) => {
+
+    const isAccepting: boolean = noam.fsm.isAcceptingState(automaton, fromState);
+
+    const toX: number = mouseX ? mouseX : (toState ? automaton.statePositions[toState].x : 0);
+    const toY: number = mouseY ? mouseY : (toState ? automaton.statePositions[toState].y : 0);
+    const radius: number = isAccepting ? 22 : 18;
+
+    const { fromX, fromY } = closestPointOnCircle(
+        automaton.statePositions[fromState].x + 22,
+        automaton.statePositions[fromState].y + 22,
+        radius, toX, toY);
+
+
+    const arrow = (x: number, y: number, angle: number) => {
+        const dx = Math.cos(angle);
+        const dy = Math.sin(angle);
+
+        return (
+            <path className="arrow" d={`M${x},${y}L${x - 8 * dx + 5 * dy},${y - 8 * dy - 5 * dx} ${x - 8 * dx - 5 * dy},${y - 8 * dy + 5 * dx}`}></path>
+        );
+    }
 
     return (
-        <></>
+        <g className="edge">
+            <path className="line" d={`M${fromX},${fromY}L${toX},${toY}`}></path>
+            {arrow(toX, toY, Math.atan2(toY - fromY, toX - fromX))}
+        </g>
     );
 }
 
@@ -71,6 +121,7 @@ export const AutomatonDesigner: React.FC<{ automaton: any, onUpdate: (automaton:
     const [selected, setSelected] = useState('');
     const [dragging, setDragging] = useState('');
     const [linking, setLinking] = useState('');
+    const [coordinates, setCoordinates] = useState({ x: 0, y: 0 });
 
     const addNewState = (e: React.MouseEvent) => {
         const state = 's' + automaton.states.length.toString();
@@ -82,11 +133,7 @@ export const AutomatonDesigner: React.FC<{ automaton: any, onUpdate: (automaton:
             automaton.statePositions = [];
         }
 
-        var rect = e.currentTarget.getBoundingClientRect();
-        automaton.statePositions[state] = {
-            x: e.clientX - rect.left - 22,
-            y: e.clientY - rect.top - 22
-        };
+        automaton.statePositions[state] = relativePositionToElement(e.currentTarget, e.clientX - 22, e.clientY - 22);
 
         if (automaton.states.length === 1) {
             noam.fsm.setInitialState(automaton, state);
@@ -110,6 +157,9 @@ export const AutomatonDesigner: React.FC<{ automaton: any, onUpdate: (automaton:
         setSelected(state);
         if (e.shiftKey) {
             setLinking(state);
+            if (e.currentTarget) {
+                setCoordinates(relativePositionToElement(e.currentTarget, e.clientX, e.clientY));
+            }
         } else {
             setDragging(state);
         }
@@ -121,6 +171,8 @@ export const AutomatonDesigner: React.FC<{ automaton: any, onUpdate: (automaton:
             automaton.statePositions[dragging].x += e.movementX;
             automaton.statePositions[dragging].y += e.movementY;
             onUpdate(automaton);
+        } else if (linking) {
+            setCoordinates(relativePositionToElement(e.currentTarget, e.clientX + 5, e.clientY + 5));
         }
     }
 
@@ -143,6 +195,22 @@ export const AutomatonDesigner: React.FC<{ automaton: any, onUpdate: (automaton:
         setDragging('');
     }
 
+    var edge = <></>;
+    if (linking) {
+        edge = <Edge automaton={automaton} fromState={linking} mouseX={coordinates.x} mouseY={coordinates.y} />;
+    }
+
+    const originalTransitions = automaton.transitions as Array<{ fromState: string, toStates: Array<string>, symbol: string }>;
+    var transitions = [];
+    for (var transition of originalTransitions) {
+        for (var toState of transition.toStates) {
+            const key: string = transition.fromState + '-' + toState + '-' + transition.symbol;
+            transitions.push(
+                <Edge key={key} automaton={automaton} fromState={transition.fromState} toState={toState} symbol={transition.symbol} />
+            );
+        }
+    }
+
     return (
         <div
             className="automaton-designer"
@@ -161,6 +229,8 @@ export const AutomatonDesigner: React.FC<{ automaton: any, onUpdate: (automaton:
                         onDragStart={dragStart}
                         onDragEnd={dragEnd} />
                 )}
+                {transitions}
+                {edge}
             </svg>
         </div>
     );
