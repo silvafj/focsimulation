33f58ef65f8a14cc7ef7af1cc221368755ba53cb
Fernando Silva <fsilva.pt@gmail.com>
2019-07-31 16:56:38 +0100

    Highlight the correct section in the side menu

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Layout, Menu } from 'antd';
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import { BrowserRouter, Route, Link, withRouter } from "react-router-dom";
 
 import RegularLanguage from '../../routes/regular-language';
 import Automata from '../../routes/automata';
@@ -8,8 +8,30 @@ import Automata from '../../routes/automata';
 import logo from '../../assets/logo.svg';
 
 import './app.css';
+import { string } from 'prop-types';
 
 const { Header, Sider, Content } = Layout;
+
+function getKeyFromLocation(pathname: string): string {
+  if (pathname === '/') {
+    return 'regular-language';
+  }
+
+  return pathname.substr(1);
+}
+
+const SiderWithRouter = withRouter(({ location, ...props }) => (
+  <Sider width="300px">
+    <Menu theme="dark" selectedKeys={[getKeyFromLocation(location.pathname)]}>
+      <Menu.Item key="regular-language">
+        <Link to="/regular-language">Regular language</Link>
+      </Menu.Item>
+      <Menu.Item key="automata">
+        <Link to="/automata">Automata</Link>
+      </Menu.Item>
+    </Menu>
+  </Sider>
+));
 
 export const App: React.FC = () => {
   return (
@@ -25,16 +47,7 @@ export const App: React.FC = () => {
             <a href="http://www.dcs.bbk.ac.uk/~michael/foc/foc.html">Course</a>
           </div>
         </Header>
-        <Sider width="300px">
-          <Menu theme="dark" defaultSelectedKeys={['1']}>
-            <Menu.Item key="1">
-              <Link to="/regular-language">Regular language</Link>
-            </Menu.Item>
-            <Menu.Item key="2">
-              <Link to="/automata">Automata</Link>
-            </Menu.Item>
-          </Menu>
-        </Sider>
+        <SiderWithRouter />
         <Content>
           <Route exact path={["/", "/regular-language"]} component={RegularLanguage} />
           <Route exact path="/automata" component={Automata} />
